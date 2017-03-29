babe8a45c605bff0890bde396f02ee2310f8a1a5
crakesh2206 <crakesh2206@gmail.com>
2017-03-29 14:57:17 +0530

    viewpager_added

--- app/src/main/java/com/naruto/dptest/MainActivity.java
+++ app/src/main/java/com/naruto/dptest/MainActivity.java
@@ -3,6 +3,10 @@ package com.naruto.dptest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,17 +14,26 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.bcgdv.asia.lib.fanmenu.FanMenuButtons;
+import com.naruto.dptest.frags.FirstFragment;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyPagerAdapter adapterViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         String rakesh;
+
         final FanMenuButtons sub = (FanMenuButtons) findViewById(R.id.myFABSubmenu);
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+
+
 //        //Display Sync status of SQLite DB
 //        DBController controller = new DBController(this);
 //        Toast.makeText(getApplicationContext(), controller.getSyncStatus(), Toast.LENGTH_LONG).show();
@@ -136,6 +149,48 @@ public class MainActivity extends AppCompatActivity {
         }
         return newGuess;
     }
+
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0: // Fragment # 0 - This will show FirstFragment
+                    return FirstFragment.newInstance(0, "Page # 1");
+                case 1: // Fragment # 0 - This will show FirstFragment different title
+                    return FirstFragment.newInstance(1, "Page # 2");
+                case 2: // Fragment # 1 - This will show SecondFragment
+                    return FirstFragment.newInstance(2, "Page # 3");
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Page " + position;
+        }
+
+    }
+
+
+
+
+
 }
 // take number
 //convert to array>>>
---
+++ app/src/main/java/com/naruto/dptest/frags/FirstFragment.java
@@ -0,0 +1,48 @@
+package com.naruto.dptest.frags;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.naruto.dptest.R;
+
+/**
+ * Created by Codefingers-1 on 29-03-2017.
+ */
+
+public class FirstFragment extends Fragment {
+    // Store instance variables
+    private String title;
+    private int page;
+
+    // newInstance constructor for creating fragment with arguments
+    public static FirstFragment newInstance(int page, String title) {
+        FirstFragment fragmentFirst = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragmentFirst.setArguments(args);
+        return fragmentFirst;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.listitem, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id._day);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
